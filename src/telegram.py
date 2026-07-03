10ea7a8af20517a74887e66ea19415069431e493
Mrbotrx <mrdarkrx41@gmail.com>
2026-07-04 03:30:37 +0600

    Update telegram.py

--- src/telegram.py
+++ src/telegram.py
@@ -3,28 +3,26 @@ import requests
 
 class Telegram:
     def __init__(self):
-        self.token = os.getenv("TELEGRAM_BOT_TOKEN")
+        token = os.getenv("TELEGRAM_BOT_TOKEN")
 
-        if not self.token:
-            raise ValueError("Missing TELEGRAM_BOT_TOKEN (Check GitHub Secrets + workflow env)")
+        if not token:
+            raise ValueError("Missing TELEGRAM_BOT_TOKEN")
 
-        self.base = f"https://api.telegram.org/bot{self.token}"
-
-    def get_updates(self):
-        return requests.get(f"{self.base}/getUpdates", timeout=15).json()
+        self.base = f"https://api.telegram.org/bot{token}"
 
     def send_message(self, chat_id, text):
-        return requests.post(
+        requests.post(
             f"{self.base}/sendMessage",
-            data={"chat_id": chat_id, "text": text},
-            timeout=15
-        ).json()
+            data={"chat_id": chat_id, "text": text}
+        )
 
     def send_file(self, chat_id, file_path, caption=""):
         with open(file_path, "rb") as f:
-            return requests.post(
+            requests.post(
                 f"{self.base}/sendDocument",
                 data={"chat_id": chat_id, "caption": caption},
-                files={"document": f},
-                timeout=30
-            ).json()
+                files={"document": f}
+            )
+
+    def get_updates(self):
+        return requests.get(f"{self.base}/getUpdates").json()
